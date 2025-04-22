e288b11d96541813ea0fed761be591fdcea0b590
Antoine Guillo <guillo.antoine93@gmail.com>
2025-04-22 11:57:42 +0200

    First component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'demo';
+  ngOnInit() {
+    console.log("Coucou !")
+  }
+
+  ngOnChanges() {
+    console.log("J'ai changé reviens !")
+  }
+
+  ngDoCheck() {
+    console.log("OK !");
+  }
+
+  ngAfterViewInit() {
+    console.log("Maintenant que c'est fini, on va tout changer !")
+  }
+
+  ngDoDestray() {
+    console.log("Game Over");
+  }
 }
